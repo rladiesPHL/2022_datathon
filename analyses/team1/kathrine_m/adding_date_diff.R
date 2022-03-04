07a8c0eb9768d87ff02e0485f2f3c9c897be5d70
kathrine-mcaulay <kathrine.mcaulay@gmail.com>
2022-03-04 08:41:04 -0500

    adding a csv file with the date diff values, no time for analysis

--- analyses/team1/kathrine_m/adding_date_diff.R
+++ analyses/team1/kathrine_m/adding_date_diff.R
@@ -15,9 +15,38 @@ care_management_clean_merge <- read_csv("analyses/team1/kathrine_m/care_manageme
                days_since_last = case_when(is.na(days_since_last) ~ 0,
                                     TRUE ~ as.numeric(days_since_last)),
                # Now count cumulative days since first assistance
-               days_since_start = cumsum(days_since_last))
+               days_since_start = cumsum(days_since_last)) %>% 
+        ungroup()
 
 # Take a quick look to see if this generated the desired output        
 care_management_clean_merge %>%         
         select(anon_id, assistance_date, days_since_last, days_since_start) %>% 
         head(n = 20L) # looks like it
+
+summary(care_management_clean_merge$days_since_last)
+
+##### Everything beyond here is untested #####
+# I don't think this code is doing what I want it to do
+# but don't have time to fix it this morning
+
+freq_up_to_monthly <- care_management_clean_merge %>% 
+        group_by(anon_id) %>% 
+        filter(days_since_last >0 & days_since_last <30)
+
+care_management_clean_merge %>% 
+        select(anon_id) %>% 
+        unique() %>% 
+        nrow() # just a reminder that there are 490 clients
+
+care_management_clean_merge %>% 
+        group_by(anon_id) %>% 
+        filter(days_since_last >0) %>% 
+        pull(anon_id) %>% 
+        unique()
+# 376 of the 490 clients used the services on more than one day
+# let's quickly look at those that did not
+# Which services were used?
+
+one_day_only <- care_management_clean_merge %>% 
+        group_by(anon_id) %>% 
+        filter(days_since_last )
